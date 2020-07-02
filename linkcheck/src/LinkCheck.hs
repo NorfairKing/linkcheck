c636f55fee88f6c2cebbe095056f41530f8da0fe
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-07-02 21:59:00 +0200

    little refactor

--- linkcheck/src/LinkCheck.hs
+++ linkcheck/src/LinkCheck.hs
@@ -32,47 +32,50 @@ linkCheck :: IO ()
 linkCheck = do
   Settings {..} <- getSettings
   man <- HTTP.newTlsManager
-  queue <- newTQueueIO :: IO (TQueue URI)
-  seen <- newTVarIO S.empty :: IO (TVar (Set URI))
-  results <- newTVarIO M.empty :: IO (TVar (Map URI HTTP.Status))
-  let go :: LoggingT IO ()
-      go = do
-        mv <- atomically $ tryReadTQueue queue
-        case mv of
-          Nothing -> pure () -- Done
-          Just uri -> do
-            alreadySeen <- (S.member uri) <$> readTVarIO seen
-            if alreadySeen
-              then do
-                logDebugN $ "Not fetching again: " <> T.pack (show uri)
-                go
-              else do
-                atomically $ modifyTVar' seen $ S.insert uri
-                case requestFromURI uri of
-                  Nothing -> do
-                    logErrorN $ "Unable to construct a request from this uri: " <> T.pack (show uri)
-                    pure () -- Just ignore
-                  Just req -> do
-                    logInfoN $ "Fetching: " <> T.pack (show uri)
-                    resp <- liftIO $ httpLbs req man
-                    let body = responseBody resp
-                    let status = responseStatus resp
-                    let sci = HTTP.statusCode status
-                    logDebugN $ "Got response for " <> T.pack (show uri) <> ": " <> T.pack (show sci)
-                    unless (200 <= sci && sci < 300) $ atomically $ modifyTVar' results $ M.insert uri status
-                    let tags = parseTagsOptions parseOptionsFast body
-                    let uris = mapMaybe (parseURIRelativeTo setUri) $ mapMaybe (fmap T.unpack . rightToMaybe . TE.decodeUtf8' . LB.toStrict) $ mapMaybe aTagHref tags :: [URI]
-                    let allSameHostAbsoluteUris = filter ((== uriAuthority setUri) . uriAuthority) uris
-                    atomically $ mapM_ (writeTQueue queue) allSameHostAbsoluteUris
-                    go
+  queue <- newTQueueIO
+  seen <- newTVarIO S.empty
+  results <- newTVarIO M.empty
   atomically $ writeTQueue queue setUri
-  runStderrLoggingT $ filterLogger (\_ ll -> ll >= setLogLevel) go
+  runStderrLoggingT $ filterLogger (\_ ll -> ll >= setLogLevel) $ worker setUri man queue seen results
   resultsList <- M.toList <$> readTVarIO results
   unless (null resultsList)
     $ die
     $ unlines
     $ map (\(uri, status) -> unwords [show uri, show status])
     $ resultsList
+
+worker :: URI -> HTTP.Manager -> TQueue URI -> TVar (Set URI) -> TVar (Map URI HTTP.Status) -> LoggingT IO ()
+worker root man queue seen results = go
+  where
+    go = do
+      mv <- atomically $ tryReadTQueue queue
+      case mv of
+        Nothing -> pure () -- Done
+        Just uri -> do
+          alreadySeen <- (S.member uri) <$> readTVarIO seen
+          if alreadySeen
+            then do
+              logDebugN $ "Not fetching again: " <> T.pack (show uri)
+              go
+            else do
+              atomically $ modifyTVar' seen $ S.insert uri
+              case requestFromURI uri of
+                Nothing -> do
+                  logErrorN $ "Unable to construct a request from this uri: " <> T.pack (show uri)
+                  pure () -- Just ignore
+                Just req -> do
+                  logInfoN $ "Fetching: " <> T.pack (show uri)
+                  resp <- liftIO $ httpLbs req man
+                  let body = responseBody resp
+                  let status = responseStatus resp
+                  let sci = HTTP.statusCode status
+                  logDebugN $ "Got response for " <> T.pack (show uri) <> ": " <> T.pack (show sci)
+                  unless (200 <= sci && sci < 300) $ atomically $ modifyTVar' results $ M.insert uri status
+                  let tags = parseTagsOptions parseOptionsFast body
+                  let uris = mapMaybe (parseURIRelativeTo root) $ mapMaybe (fmap T.unpack . rightToMaybe . TE.decodeUtf8' . LB.toStrict) $ mapMaybe aTagHref tags :: [URI]
+                  let allSameHostAbsoluteUris = filter ((== uriAuthority root) . uriAuthority) uris
+                  atomically $ mapM_ (writeTQueue queue) allSameHostAbsoluteUris
+                  go
 
 parseURIRelativeTo :: URI -> String -> Maybe URI
 parseURIRelativeTo root s =
