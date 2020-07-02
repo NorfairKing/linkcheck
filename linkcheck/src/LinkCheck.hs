f04b1dfa1786eba4583c0cbd0fa14a67ec520cd9
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-07-02 21:45:42 +0200

    nice logging

--- linkcheck/src/LinkCheck.hs
+++ linkcheck/src/LinkCheck.hs
@@ -9,6 +9,8 @@ where
 
 import Control.Concurrent.STM
 import Control.Monad
+import Control.Monad.IO.Class
+import Control.Monad.Logger
 import qualified Data.ByteString.Lazy as LB
 import qualified Data.Map as M
 import Data.Map (Map)
@@ -33,20 +35,26 @@ linkCheck = do
   queue <- newTQueueIO :: IO (TQueue URI)
   seen <- newTVarIO S.empty :: IO (TVar (Set URI))
   results <- newTVarIO M.empty :: IO (TVar (Map URI HTTP.Status))
-  let go = do
-        mv <- atomically $ tryReadTQueue queue
+  let go :: LoggingT IO ()
+      go = do
+        mv <- liftIO $ atomically $ tryReadTQueue queue
         case mv of
           Nothing -> pure () -- Done
           Just uri -> do
-            alreadySeen <- (S.member uri) <$> readTVarIO seen
+            alreadySeen <- liftIO $ (S.member uri) <$> readTVarIO seen
             if alreadySeen
-              then go
+              then do
+                logDebugN $ "Not fetching again: " <> T.pack (show uri)
+                go
               else do
-                atomically $ modifyTVar' seen $ S.insert uri
+                liftIO $ atomically $ modifyTVar' seen $ S.insert uri
                 case requestFromURI uri of
-                  Nothing -> die $ "Invalid uri: " <> show setUri
+                  Nothing -> do
+                    logErrorN $ "Unable to construct a request from this uri: " <> T.pack (show uri)
+                    pure () -- Just ignore
                   Just req -> do
-                    body <- withResponse req man $ \resp -> do
+                    logInfoN $ "Fetching: " <> T.pack (show uri)
+                    body <- liftIO $ withResponse req man $ \resp -> do
                       bodyChunks <- brConsume $ responseBody resp
                       let status = responseStatus resp
                       let sci = HTTP.statusCode status
@@ -55,10 +63,10 @@ linkCheck = do
                     let tags = parseTagsOptions parseOptionsFast body
                     let uris = mapMaybe (parseURIRelativeTo setUri) $ mapMaybe (fmap T.unpack . rightToMaybe . TE.decodeUtf8' . LB.toStrict) $ mapMaybe aTagHref tags :: [URI]
                     let allSameHostAbsoluteUris = filter ((== uriAuthority setUri) . uriAuthority) uris
-                    atomically $ mapM_ (writeTQueue queue) allSameHostAbsoluteUris
+                    liftIO $ atomically $ mapM_ (writeTQueue queue) allSameHostAbsoluteUris
                     go
   atomically $ writeTQueue queue setUri
-  go
+  runStderrLoggingT go
   resultsList <- M.toList <$> readTVarIO results
   unless (null resultsList)
     $ die
