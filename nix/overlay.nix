bd160b21264e83bb83ce2019f672558d4ef8ed9c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2024-07-29 12:50:12 +0200

    update inputs

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -3,15 +3,12 @@ with final.haskell.lib;
 
 {
   linkcheck = justStaticExecutables final.haskellPackages.linkcheck;
-  haskellPackages =
-    prev.haskellPackages.override (old:
-      {
-        overrides = final.lib.composeExtensions (old.overrides or (_: _: { }))
-          (
-            self: super: {
-              linkcheck = self.opt-env-conf.installManpagesAndCompletions [ "linkcheck" ] (buildStrictly (self.callPackage ../linkcheck { }));
-            }
-          );
+  haskellPackages = prev.haskellPackages.override (old: {
+    overrides = final.lib.composeExtensions (old.overrides or (_: _: { })) (
+      self: super: {
+        linkcheck = self.opt-env-conf.installManpagesAndCompletions [ "linkcheck" ] (buildStrictly (self.callPackage ../linkcheck { }));
       }
     );
+  }
+  );
 }
